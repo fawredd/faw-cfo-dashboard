96b0f9f69e2cb8cf2f2ff1d1f55237ea5c2ad649
fawredd <moore.marcos@gmail.com>
2024-05-13 09:55:31 -0300

    [FEAT] Gauge chart

--- components/gaugeChart.tsx
+++ components/gaugeChart.tsx
@@ -1,14 +1,52 @@
-'use client'
-import dynamic from "next/dynamic"
-
-const Charts = () => {
-    return (
-      <>
-        <section>
-          test
-        </section>
-      </>
-    )
+"use client";
+import { PieChart, Pie, Cell } from "recharts"
+const data01 = [
+  {
+    "name": "Group A",
+    "value": 400
+  },
+  {
+    "name": "Group B",
+    "value": 300
+  },
+  {
+    "name": "Group C",
+    "value": 300
+  },
+  {
+    "name": "Group D",
+    "value": 200
+  },
+  {
+    "name": "Group E",
+    "value": 278
+  },
+  {
+    "name": "Group F",
+    "value": 189
   }
-  
-  export default Charts;
+]
+const GaugeChart = () => {
+  return (
+    <>
+      <PieChart height={260} width={500}>
+        <Pie
+          startAngle={180}
+          endAngle={0}
+          innerRadius="55%"
+          data={data01}
+          dataKey="value"
+          labelLine={true}
+          blendStroke
+          isAnimationActive={false}
+          cy={"70%"}
+        >
+          <Cell fill="#000" />
+          <Cell fill="#eaeaea" />
+        </Pie>
+      </PieChart>
+    </>
+  );
+};
+
+export default GaugeChart;
--- components/keyMetrics.tsx
+++ components/keyMetrics.tsx
@@ -3,6 +3,7 @@ import performLoginSos from "@/lib/sos-contador/sos-login";
 import { cuitSyS } from "@/lib/sos-contador/sos-sys";
 import  kpis  from "@/lib/sos-contador/setMetrics"
 import AreaChartPlot from "./areaChartPlot";
+import GaugeChart from "./gaugeChart";
 
 export default async function KeyMetrics() {
   const jwtC = await performLoginSos(env.CUIT);
@@ -12,6 +13,9 @@ export default async function KeyMetrics() {
     <>
       <div className="text-sm h-96 w-96">
         <AreaChartPlot />
+      </div>
+      <div className="text-sm h-96 w-96">
+        <GaugeChart />
       </div>
     </>
   );
