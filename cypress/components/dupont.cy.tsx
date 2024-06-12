726eb532a0a2c3590c851fb11aa9b2508fb4bcb1
fawredd <moore.marcos@gmail.com>
2024-06-12 09:26:54 -0300

    BUGS

--- cypress/components/dupont.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Dupont from '../../components/dupont'
-
-describe('<Dupont />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Dupont />)
-  })
-})
